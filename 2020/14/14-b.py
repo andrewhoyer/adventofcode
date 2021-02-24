4ec5107050625f9d30bb3bac2509d9d02b58738d
Andrew Hoyer <andrewhoyer@gmail.com>
2021-02-24 09:27:51 -0800

    Day 14 solution, part 2

---
+++ 2020/14/14-b.py
@@ -0,0 +1,71 @@
+import re
+import copy
+
+inputdata = open("input-data.txt", "r").readlines()
+
+mask           = ''
+maskcharacters = []
+values         = {}
+
+def setvaluesformemoryaddresses(binaryarray, startingindex, memvalue, depth):
+
+	# Indenting really helps with debugging, to show search depth level
+	spaces = ''
+	for _ in range(depth):
+		spaces = spaces + '  '		
+	
+	if startingindex > len(binaryarray) - 1:
+		#print("{}set, array {}, startingindex {}, memvalue {}".format(spaces, binaryarray, startingindex, memvalue))
+		#print("{}reached end. [{}] = {}".format(spaces, int("".join(binaryarray), 2), memvalue))
+		values[int("".join(binaryarray), 2)] = memvalue	
+	else:
+		if binaryarray[startingindex] == 'X':
+			# Where the value is X, start recursion using a 0 and 1
+			for i in range(0,2):
+				binarycopy = copy.deepcopy(binaryarray)
+				binarycopy[startingindex] = str(i)
+				setvaluesformemoryaddresses(binarycopy, startingindex + 1, memvalue, depth + 1)
+		else:
+			# For 0 and 1, just move one level deeper
+			binarycopy = copy.deepcopy(binaryarray)
+			setvaluesformemoryaddresses(binarycopy, startingindex + 1, memvalue, depth + 1)
+
+
+for input in inputdata:
+	
+	action = re.search(r'([a-zA-Z]+)', input.strip()).group()
+	
+	if action == 'mask':
+		
+		mask = input.strip().split(' = ')[1]
+		maskcharacters = [char for char in mask]
+		
+	else:
+		
+		process  = input.strip().split(' = ')
+		memid    = int(re.search(r'([0-9]+)', process[0]).group())
+		memvalue = int(process[1])
+		binary   = [char for char in bin(memid).replace('0b', '')]
+
+		# Create 36-character string by adding '0' padding
+		for i in range(0, len(maskcharacters) - len(binary)):
+			binary.insert(0, '0')
+		
+		# Masking rules		
+		for i in range(0, len(maskcharacters)):
+			if maskcharacters[i] == '0':
+				pass
+			elif maskcharacters[i] == '1':
+				binary[i] = maskcharacters[i]
+			else:
+				binary[i] = 'X'
+		
+		# Pass the value to the recurring function for processing
+		setvaluesformemoryaddresses(binary, 0, memvalue, 0)
+
+		
+total = 0
+for value in values.values():
+		total = total + value
+		
+print("\nSum of values after mask processing: {}".format(total))
--- 2020/14/README.md
+++ 2020/14/README.md
@@ -2,6 +2,16 @@
 
 [https://adventofcode.com/2020/day/14](https://adventofcode.com/2020/day/14)
 
+This puzzle involves conversion of decimal and binary numbers, and the use of masking to generate values.
+
 ```
 $ python3 14.py
+```
+
+## Part 2
+
+The second part of this puzzle presents an interesting problem, where multiple memory addresses can be generated after the masking rules are applied. One way to solve it is to use a recurring function, which is what I did here. I'd be interested in finding out what other methods developers used, and if there are linear alternatives.
+
+```
+$ python3 14-b.py
 ```
