c583816957f7cbad7a6ba1c6dfa7739e0636d7a8
admin <andrewhoyer@gmail.com>
2024-10-25 12:49:58 -0700

    Added Part 2 solution

--- 2023/01/solve.php
+++ 2023/01/solve.php
@@ -1,34 +1,95 @@
 <?php
 
-$filename   = 'data-input.txt';
+$filename   = $argv[1];
 $handle     = fopen($filename, 'r');
 $lines      = explode("\n", fread($handle, filesize($filename)));
 
 fclose($handle);
 
-$total = 0;
+$text_numbers = array(
+    'one' => 1,
+    'two' => 2,
+    'three' => 3,
+    'four' => 4,
+    'five' => 5,
+    'six' => 6,
+    'seven' => 7,
+    'eight' => 8,
+    'nine' => 9
+);
+
+$result_part_1 = 0;
+$result_part_2 = 0;
 
 foreach ($lines as $line) {
-    $first  = '';
-    $last   = '';
+
+    // For the benefit of Part 2, the default first and last positions are 
+    // set to be on the extreme oppsite ends. It's especially important for cases 
+    // where no numbers appear in a row.
+    $first      = '';
+    $last       = '';
+    $first_pos  = strlen($line);
+    $last_pos   = -1;
 
     for ($i = 0; $i < strlen($line); $i++) {
         if (is_numeric($line[$i])) {
             if ($first == '') {
-                $first = $line[$i];
+                $first      = $line[$i];
+                $first_pos  = $i;
             } else {
-                $last = $line[$i];
+                $last       = $line[$i];
+                $last_pos   = $i;
             }
         }
     }
 
-    // In the unlikely event there's only a single number in the row, use it for both digits
-    if ($last == '') {
-        $last = $first;
-    }
+    // For Part 1, if no numbers are found, we have to skip the calculation on that row
+    if ($first != '') {
+        
+        // In the event there's only a single number in the row, use it for both digits
+        if ($last == '') {
+            $last       = $first;
+            $last_pos   = $first_pos;
+        }
     
-    $total += (int)($first . $last);
+        // Calculate result for part 1 using just the numbers found in the string
+        $result_part_1 += (int)($first . $last);
+    }
+
+    // Part 2
+    // Search for each number word and update the first and last values if the word
+    // appears before or after the latest known first or last position.
+    foreach (array_keys($text_numbers) as $key) {
+
+        // Search from the start of the line
+        $position = strpos($line, $key);
+
+        if ($position === false) {
+            // Do nothing
+        } else {
+            if ($position < $first_pos) {
+                $first      = $text_numbers[$key];
+                $first_pos  = $position;
+            }
+        }
+
+        // Search from the end of the line
+        $position = strrpos($line, $key);
+
+        if ($position === false) {
+            // Do nothing
+        } else {
+            if ($position > $last_pos) {
+                $last      = $text_numbers[$key];
+                $last_pos  = $position;
+            }
+        }        
+        
+    }
+
+    $result_part_2 += (int)($first . $last);
 
 }
 
-echo $total;
+echo "Solution for Part 1: {$result_part_1}\n";
+echo "Solution for Part 2: {$result_part_2}\n";
