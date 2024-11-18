a272990f81e13a6fbc10b8bb5aa595cde9e9179a
admin <andrewhoyer@gmail.com>
2024-11-18 10:06:18 -0800

    API code

---
+++ API/README.md
@@ -0,0 +1,27 @@
+# Advent of Code API
+
+You can use the Advent of Code API to dynamically create content for private leaderboards. This is especially useful for things like a daily update during any year's December event.
+
+**What you'll need**
+
+* The leaderboard ID. You'll find this in the URL when viewing your private leaderboard.
+* The year you want to report data from. This will usually be the current year, but it can be used for any event going back to 2015.
+* The session cookie. It's a long string which you can find using browser debug tools. See this [GitHub issue](https://github.com/wimglenn/advent-of-code-wim/issues/1) for a nice screenshot that shows you where to look.
+
+All you need to do now is plug these values into one of the scripts here and start using them.
+
+Please note that it is requested that the Advent of Code API be used no more frequently than 15 minutes.
+
+## Python
+
+Use the `leaderboard-by-score.py` script if you want to use the API directly from the command line, on a server, a cron job, etc.
+
+```python3 leaderboard-by-score.py```
+
+Note that the output is formatted with markdown meant for posting to a Discord server. Alter the formatting for whatever your target audience is.
+
+## Zapier
+
+A version of the script is available in `leaderboard-by-score-zapier.py` that is meant to be used in a Zapier Code action. No inputs are necessary, just create the action, select Python, copy and paste the code, and plug in the values at the top of the script.
+
+Note that the output is formatted with markdown meant for posting to a Discord server. Alter the output formatting for whatever the following actions require.
---
+++ API/leaderboard-by-score-zapier.py
@@ -0,0 +1,69 @@
+import requests
+
+year            = "" # Event year, ie. 2024
+leaderboard_id  = "" # Leaderboard ID. Likely a 7-digit number found in leaderboard URLs.
+cookie          = "" # The Cookie session ID. 
+
+url = "https://adventofcode.com/" + year + "/leaderboard/private/view/" + leaderboard_id + ".json"
+
+# Set the session cookie.
+cookies = {
+    'session': cookie
+}
+
+session = requests.Session()
+session.cookies.update(cookies)
+
+try:
+    
+    response = session.get(url)
+
+    if response.status_code == 200:
+        
+        data = response.json()
+
+        members = []
+
+        for key in data['members'].keys():
+            
+            member = {'name': data['members'][key]['name'], 'score': data['members'][key]['local_score'], 'stars': data['members'][key]['stars']}
+            
+            if len(members) == 0:
+                members.append(member)
+            else:
+                inserted = False
+                for i in range(0, len(members)):
+                    if member['score'] >= members[i]['score']:
+                        members.insert(i, member)
+                        inserted = True
+                        break
+                
+                if inserted == False:
+                    members.append(member)
+                
+        output  = "### Daily AoC " + year + " Results\n"
+        counter = 0
+        
+        for member in members:
+            icon = "🏅"
+            if counter == 0:
+                icon = "🥇"
+            elif counter == 1:
+                icon = "🥈"
+            elif counter == 2:
+                icon = "🥉"
+                
+            output += "**" + member['name'] + "** " + icon + " " + str(member['score']) + ", ⭐️ " + str(member['stars']) + "\n"
+            counter = counter + 1
+        
+        return {'output': output}
+        
+    else:
+        # Handle an unsuccessful request code
+        return {'output': "Daily AoC Results: Request failed."}
+
+except requests.RequestException as e:
+    # This block handles network errors or server issues
+    return {'output': "Daily AoC Results: An error occurred."}
+
+session.close()
