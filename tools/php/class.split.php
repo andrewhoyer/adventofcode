884e99a7ffe7dbbd1e084959b1dac2d121740389
admin <andrewhoyer@gmail.com>
2025-11-05 14:13:19 -0800

    Added str_to_array function

--- tools/php/class.split.php
+++ tools/php/class.split.php
@@ -6,6 +6,16 @@ class Splitter {
 		return explode($delimiter, $str);
 	}
 
+	public function str_to_array(string $str): array {
+		$chars = [];
+
+		for ($i = 0;$i < strlen($str);$i++) {
+			$chars[] = substr($str, $i, 1);
+		}
+
+		return $chars;
+	}
+
 }
 
 ?>
